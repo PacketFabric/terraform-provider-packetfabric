941aafca744f5bdbbcb258759f9d6733b2bad4d5
Yrineu Rodrigues <yrineu@pontovinte.com>
2022-08-23 19:26:57 -0400

    Fix Issue: #66: OutboundCrossConnect not found.
    Signed-off-by: Yrineu Rodrigues <yrineu@pontovinte.com>

--- internal/provider/resource_outbound_cross_connect.go
+++ internal/provider/resource_outbound_cross_connect.go
@@ -166,9 +166,11 @@ func resourceOutboundCrossConnectDelete(ctx context.Context, d *schema.ResourceD
 	c := m.(*packetfabric.PFClient)
 	c.Ctx = ctx
 	var diags diag.Diagnostics
-	err := c.DeleteOutboundCrossConnect(d.Id())
-	if err != nil {
-		return diag.FromErr(err)
+	if port, ok := d.GetOk("port"); ok {
+		err := c.DeleteOutboundCrossConnect(port.(string))
+		if err != nil {
+			return diag.FromErr(err)
+		}
 	}
 	return diags
 }
