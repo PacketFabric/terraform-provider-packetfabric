78fae719239e8bef2d5fd660c424ed1a99a8b2e5
Yrineu Rodrigues <yrineu@pontovinte.com>
2022-10-04 12:39:09 -0400

    Add resource 'packetfabric_cloud_router_attach_customer_owned_port'.
    Signed-off-by: Yrineu Rodrigues <yrineu@pontovinte.com>

---
+++ internal/packetfabric/cloud_router_port.go
@@ -0,0 +1,56 @@
+package packetfabric
+
+import "fmt"
+
+const attachCustomerPortToCRURI = "/v2/services/cloud-routers/%s/connections/packetfabric"
+
+type CustomerOwnedPort struct {
+	AccountUUID            string `json:"account_uuid,omitempty"`
+	MaybeNat               bool   `json:"maybe_nat,omitempty"`
+	PortCircuitID          string `json:"port_circuit_id,omitempty"`
+	Description            string `json:"description,omitempty"`
+	Untagged               bool   `json:"untagged,omitempty"`
+	Vlan                   int    `json:"vlan,omitempty"`
+	Speed                  string `json:"speed,omitempty"`
+	IsPublic               bool   `json:"is_public,omitempty"`
+	PublishedQuoteLineUUID string `json:"published_quote_line_uuid,omitempty"`
+}
+
+type CustomerOwnedPortResp struct {
+	PortType                  string        `json:"port_type,omitempty"`
+	ConnectionType            string        `json:"connection_type,omitempty"`
+	PortCircuitID             string        `json:"port_circuit_id,omitempty"`
+	PendingDelete             bool          `json:"pending_delete,omitempty"`
+	Deleted                   bool          `json:"deleted,omitempty"`
+	Speed                     string        `json:"speed,omitempty"`
+	State                     string        `json:"state,omitempty"`
+	CloudCircuitID            string        `json:"cloud_circuit_id,omitempty"`
+	AccountUUID               string        `json:"account_uuid,omitempty"`
+	ServiceClass              string        `json:"service_class,omitempty"`
+	ServiceProvider           string        `json:"service_provider,omitempty"`
+	ServiceType               string        `json:"service_type,omitempty"`
+	Description               string        `json:"description,omitempty"`
+	UUID                      string        `json:"uuid,omitempty"`
+	CloudProviderConnectionID string        `json:"cloud_provider_connection_id,omitempty"`
+	CloudSettings             CloudSettings `json:"cloud_settings,omitempty"`
+	UserUUID                  string        `json:"user_uuid,omitempty"`
+	CustomerUUID              string        `json:"customer_uuid,omitempty"`
+	TimeCreated               string        `json:"time_created,omitempty"`
+	TimeUpdated               string        `json:"time_updated,omitempty"`
+	CloudProvider             CloudProvider `json:"cloud_provider,omitempty"`
+	Pop                       string        `json:"pop,omitempty"`
+	Site                      string        `json:"site,omitempty"`
+	BgpState                  string        `json:"bgp_state,omitempty"`
+	CloudRouterCircuitID      string        `json:"cloud_router_circuit_id,omitempty"`
+	NatCapable                bool          `json:"nat_capable,omitempty"`
+}
+
+func (c *PFClient) AttachCustomerOwnedPortToCR(ownedPort CustomerOwnedPort, cID string) (*CustomerOwnedPortResp, error) {
+	formatedURI := fmt.Sprintf(attachCustomerPortToCRURI, cID)
+	expectedResp := &CustomerOwnedPortResp{}
+	_, err := c.sendRequest(formatedURI, postMethod, ownedPort, expectedResp)
+	if err != nil {
+		return nil, err
+	}
+	return expectedResp, nil
+}
