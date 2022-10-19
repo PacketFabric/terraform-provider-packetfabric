b4add16227c99f991ce1b872dbd2d4f4a3a42344
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-10-19 15:18:02 -0700

    Update all use cases examples (split tf files)

---
+++ examples/use-cases/cloud_router_aws/random.tf
@@ -0,0 +1,2 @@
+# Create random name to use to name objects
+resource "random_pet" "name" {}
--- examples/use-cases/cloud_router_google_ipsec/provider.tf
+++ examples/use-cases/cloud_router_google_ipsec/provider.tf
@@ -24,6 +24,3 @@ provider "google" {
   region      = var.gcp_region1
   zone        = var.gcp_zone1
 }
-
-# create random name to use to name objects
-resource "random_pet" "name" {}
