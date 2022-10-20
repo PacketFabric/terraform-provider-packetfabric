d87e01036bfe6059ceee6e00acfd7c0ce604d94b
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-10-19 20:36:30 -0700

    Update all use cases examples (added transit gw example for AWS and more)

---
+++ examples/use-cases/cloud_router_aws/aws_vpc.tf
@@ -0,0 +1,63 @@
+# Create the VPCs
+resource "aws_vpc" "vpc_1" {
+  provider             = aws
+  cidr_block           = var.vpc_cidr1
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.tag_name}-${random_pet.name.id}"
+  }
+}
+resource "aws_vpc" "vpc_2" {
+  provider             = aws.region2
+  cidr_block           = var.vpc_cidr2
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.tag_name}-${random_pet.name.id}"
+  }
+}
+
+# Define the subnets
+resource "aws_subnet" "subnet_1" {
+  provider   = aws
+  vpc_id     = aws_vpc.vpc_1.id
+  cidr_block = var.subnet_cidr1
+  tags = {
+    Name = "${var.tag_name}-${random_pet.name.id}"
+  }
+}
+resource "aws_subnet" "subnet_2" {
+  provider   = aws.region2
+  vpc_id     = aws_vpc.vpc_2.id
+  cidr_block = var.subnet_cidr2
+  tags = {
+    Name = "${var.tag_name}-${random_pet.name.id}"
+  }
+}
+
+# Define the internet gateways
+resource "aws_internet_gateway" "gw_1" {
+  provider = aws
+  vpc_id   = aws_vpc.vpc_1.id
+  tags = {
+    Name = "${var.tag_name}-${random_pet.name.id}"
+  }
+}
+resource "aws_internet_gateway" "gw_2" {
+  provider = aws.region2
+  vpc_id   = aws_vpc.vpc_2.id
+  tags = {
+    Name = "${var.tag_name}-${random_pet.name.id}"
+  }
+}
+
+# Assign the route table to the subnet
+resource "aws_route_table_association" "route_association_1" {
+  provider       = aws
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.route_table_1.id
+}
+resource "aws_route_table_association" "route_association_2" {
+  provider       = aws.region2
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.route_table_2.id
+}
