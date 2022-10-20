a0f54031c02f0485051b6ffe5471c36080312049
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-10-19 20:37:42 -0700

    Update all use cases examples (added transit gw example for AWS and more)

---
+++ examples/use-cases/cloud_router_aws/aws_dx_gateways.tf
@@ -0,0 +1,17 @@
+# From the AWS side: Create a gateway
+resource "aws_dx_gateway" "direct_connect_gw_1" {
+  provider        = aws
+  name            = "${var.tag_name}-${random_pet.name.id}-${var.pf_crc_pop1}"
+  amazon_side_asn = var.amazon_side_asn1
+  depends_on = [
+    packetfabric_cloud_router_connection_aws.crc_1
+  ]
+}
+resource "aws_dx_gateway" "direct_connect_gw_2" {
+  provider        = aws.region2
+  name            = "${var.tag_name}-${random_pet.name.id}-${var.pf_crc_pop2}"
+  amazon_side_asn = var.amazon_side_asn2
+  depends_on = [
+    packetfabric_cloud_router_connection_aws.crc_2
+  ]
+}
