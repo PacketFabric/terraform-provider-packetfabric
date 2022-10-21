88bceca604de33d6ba6017b084b007d3e1b9e635
Yrineu Rodrigues <yrineu@pontovinte.com>
2022-10-20 22:15:59 -0400

    #164: Fix incorrect naming in the code.
    Signed-off-by: Yrineu Rodrigues <yrineu@pontovinte.com>

--- internal/packetfabric/aws_backbone.go
+++ internal/packetfabric/aws_backbone.go
@@ -4,7 +4,7 @@ import "fmt"
 
 const awsBackStatusURI = "/v2.1/services/%s/status"
 
-func (c *PFClient) GetAwsBackboneState(vcCircuitID string) (*ServiceState, error) {
+func (c *PFClient) GetBackboneState(vcCircuitID string) (*ServiceState, error) {
 	formatedURI := fmt.Sprintf(awsBackStatusURI, vcCircuitID)
 	expectedResp := &ServiceState{}
 	_, err := c.sendRequest(formatedURI, getMethod, nil, expectedResp)
