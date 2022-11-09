f530ef87b9e64840c7820ab0209caef2d47d11ef
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-11-08 16:33:31 -0800

    fix oracle and google hosted cloud import

--- internal/provider/resource_google_hosted_mkt_conn.go
+++ internal/provider/resource_google_hosted_mkt_conn.go
@@ -76,9 +76,6 @@ func resourceGoogleHostedMktConn() *schema.Resource {
 				Description:  "The speed of the new connection.\n\n\tEnum: [\"50Mbps\", \"100Mbps\", \"200Mbps\", \"300Mbps\", \"400Mbps\", \"500Mbps\", \"1Gbps\", \"2Gbps\", \"5Gbps\", \"10Gbps\"]",
 			},
 		},
-		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
-		},
 	}
 }
 
