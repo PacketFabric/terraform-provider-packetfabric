799099051f316f9753432055e9fd13c064ce2d88
elXaptation <el@xaptation.com>
2022-11-28 12:40:10 -0800

    Examples and Docs for resource packetfabric_port_loa

---
+++ examples/resources/packetfabric_port_loa/resource.tf
@@ -0,0 +1,6 @@
+resource "packetfabric_port_loa" "new" {
+  provider = packetfabric
+  port_circuit_id = var.pf_market_port_circuit_id
+  loa_customer_name = "MyName"
+  destination_email = "email@mydomain.com"
+}
