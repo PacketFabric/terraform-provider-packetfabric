2b17e81b29a2358c71abc500ef023caeb2135b49
elXaptation <el@xaptation.com>
2022-11-28 11:35:47 -0800

    remove account_uuid from examples

--- examples/resources/packetfabric_cloud_router_connection_google/resource.tf
+++ examples/resources/packetfabric_cloud_router_connection_google/resource.tf
@@ -10,7 +10,6 @@ resource "packetfabric_cloud_router_connection_google" "crc2" {
   provider                    = packetfabric
   description                 = var.pf_crc_description
   circuit_id                  = packetfabric_cloud_router.cr1.id
-  account_uuid                = var.pf_account_uuid
   google_pairing_key          = var.pf_crc_google_pairing_key
   google_vlan_attachment_name = var.pf_crc_google_vlan_attachment_name
   pop                         = var.pf_crc_pop
