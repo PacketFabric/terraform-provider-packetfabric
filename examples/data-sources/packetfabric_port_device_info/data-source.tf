537a6afd5e341f56bf2b461f4e8ee94035166253
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-11-29 09:42:02 -0800

    run fmt on the examples and update docs/examples

--- examples/data-sources/packetfabric_port_device_info/data-source.tf
+++ examples/data-sources/packetfabric_port_device_info/data-source.tf
@@ -1,8 +1,7 @@
 data "packetfabric_port_device_info" "port_device_info_1" {
-  provider = packetfabric
+  provider        = packetfabric
   port_circuit_id = var.pf_port_circuit_id
 }
-
 output "packetfabric_port_device_info" {
   value = data.packetfabric_port_device_info.port_device_info_1
 }
