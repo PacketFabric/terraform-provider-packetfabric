ca495fb812da90980567784a78b68f8b9fabc074
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-11-29 09:18:36 -0800

    rename packetfabric_locations_market to packetfabric_locations_markets

--- examples/data-sources/packetfabric_locations_market/data-source.tf
+++
@@ -1,7 +0,0 @@
-data "packetfabric_locations_market" "locations_market_1" {
-  provider = packetfabric
-}
-
-output "packetfabric_locations_market" {
-  value = data.packetfabric_locations_market.locations_market_1
-}
