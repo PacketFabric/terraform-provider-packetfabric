4f5615c1bdbb09e066b8b78b3c1009dfd4a93ddc
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-11-29 09:35:36 -0800

    update examples

--- examples/data-sources/packetfabric_cs_dedicated_connections/data-source.tf
+++ examples/data-sources/packetfabric_cs_dedicated_connections/data-source.tf
@@ -1,7 +1,6 @@
 data "packetfabric_cs_dedicated_connections" "current" {
   provider = packetfabric
 }
-
 output "packetfabric_cs_dedicated_connections" {
   value = data.packetfabric_cs_dedicated_connections.current
 }
--- examples/data-sources/packetfabric_port_vlans/data-source.tf
+++ examples/data-sources/packetfabric_port_vlans/data-source.tf
@@ -1,8 +1,7 @@
 data "packetfabric_port_vlans" "port_vlans_1" {
-  provider = packetfabric
+  provider        = packetfabric
   port_circuit_id = var.pf_port_circuit_id
 }
-
 output "packetfabric_port_vlans" {
   value = data.packetfabric_port_vlans.port_vlans_1
 }
