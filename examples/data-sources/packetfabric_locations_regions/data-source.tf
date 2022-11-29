96cc6ca7ccbd56b68f4dbfafab2b0351f8c9e942
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-11-29 10:04:52 -0800

    update examples/docs

--- examples/data-sources/packetfabric_locations_regions/data-source.tf
+++ examples/data-sources/packetfabric_locations_regions/data-source.tf
@@ -1,7 +1,6 @@
 data "packetfabric_locations_regions" "locations_regions_1" {
   provider = packetfabric
 }
-
 output "packetfabric_locations_regions" {
   value = data.packetfabric_locations_regions.locations_regions_1
 }
