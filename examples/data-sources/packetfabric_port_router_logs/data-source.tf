9157a2256216b151345d74085c5429f241756d28
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-12-01 08:53:30 -0800

    run terraform fmt -recursive on examples

--- examples/data-sources/packetfabric_port_router_logs/data-source.tf
+++ examples/data-sources/packetfabric_port_router_logs/data-source.tf
@@ -1,10 +1,9 @@
 data "packetfabric_port_router_logs" "port_logs_1" {
-  provider = packetfabric
+  provider        = packetfabric
   port_circuit_id = var.pf_port_circuit_id
-  time_from = "2022-11-30 00:00:00"
-  time_to = "2022-12-01 00:00:00"
+  time_from       = "2022-11-30 00:00:00"
+  time_to         = "2022-12-01 00:00:00"
 }
-
 output "packetfabric_port_router_logs" {
   value = data.packetfabric_port_router_logs.port_logs_1
 }
