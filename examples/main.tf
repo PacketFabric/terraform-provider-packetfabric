414c353021d2f184d36605be1aa7e939ee2ed8a8
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-01-03 17:46:41 -0800

    add bgp data source

--- examples/main.tf
+++ examples/main.tf
@@ -774,6 +774,13 @@ resource "random_pet" "name" {}
 #   value = data.packetfabric_cloud_router_connections.all_crc
 # }
 
+# data "packetfabric_cloud_router_bgp_session" "all_cr_bgp_sessions" {
+#   provider = packetfabric
+# }
+# output "packetfabric_cloud_router_bgp_session" {
+#   value = data.packetfabric_cloud_router_bgp_session.all_cr_bgp_sessions
+# }
+
 # #######################################
 # ##### Billing
 # #######################################
