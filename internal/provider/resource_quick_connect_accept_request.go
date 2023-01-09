acb06a53fb041df645b78c94b86d8ee5ce15c203
Yrineu Rodrigues <yrineu@pontovinte.com>
2023-01-08 19:08:39 -0500

    253: Feature resource packetfabric_quick_connect_accept_request.
    Signed-off-by: Yrineu Rodrigues <yrineu@pontovinte.com>

---
+++ internal/provider/resource_quick_connect_accept_request.go
@@ -0,0 +1,65 @@
+package provider
+
+import (
+	"context"
+	"time"
+
+	"github.com/PacketFabric/terraform-provider-packetfabric/internal/packetfabric"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func resourceQuickConnectAcceptRequest() *schema.Resource {
+	return &schema.Resource{
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Read:   schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+		CreateContext: resourceAcceptRequestCreate,
+		ReadContext:   resourceAcceptRequestRead,
+		UpdateContext: resourceAcceptRequestUpdate,
+		DeleteContext: resourceAcceptRequestDelete,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"circuit_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "Circuit ID of the Cloud Router third-party service import.",
+			},
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+
+func resourceAcceptRequestCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*packetfabric.PFClient)
+	c.Ctx = ctx
+	var diags diag.Diagnostics
+	if circuitID, ok := d.GetOk("circuit_id"); ok {
+		if _, err := c.AcceptCloudRouterService(circuitID.(string)); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	return diags
+}
+
+func resourceAcceptRequestRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return diag.Diagnostics{}
+}
+
+func resourceAcceptRequestUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return diag.Diagnostics{}
+}
+
+func resourceAcceptRequestDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return diag.Diagnostics{}
+}
