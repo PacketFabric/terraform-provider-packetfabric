3c727c230f13554e19d47a2b785676306625453e
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-01-16 17:34:44 -0800

    renaming port marketplace resources and data-sources

--- examples/data-sources/packetfabric_marketplace_service_requests/data-source.tf
+++ examples/data-sources/packetfabric_marketplace_service_requests/data-source.tf
@@ -1,7 +1,7 @@
-data "packetfabric_marketplace_service_requests" "marketplace_requests" {
+data "packetfabric_marketplace_service_port_requests" "marketplace_requests" {
   provider = packetfabric
   type     = "sent" # sent or received
 }
-output "packetfabric_marketplace_service_requests" {
-  value = data.packetfabric_marketplace_service_requests.marketplace_requests
+output "packetfabric_marketplace_service_port_requests" {
+  value = data.packetfabric_marketplace_service_port_requests.marketplace_requests
 }
