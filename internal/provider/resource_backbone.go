e24936406fccaf9817b9ab2ea18233e788568529
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-01-31 00:17:32 -0800

    resourceServiceSettingsUpdate update

--- internal/provider/resource_backbone.go
+++ internal/provider/resource_backbone.go
@@ -183,8 +183,11 @@ func resourceServiceSettingsUpdate(ctx context.Context, d *schema.ResourceData, 
 	c.Ctx = ctx
 	var diags diag.Diagnostics
 
-	if d.HasChange("longhaul_type") || d.HasChange("port_circuit_id") || d.HasChange("epl") {
-		return diag.Errorf("longhaul_type or port_circuit_id cannot be updated")
+	if d.HasChange("bandwidth.0.longhaul_type") ||
+		d.HasChange("interface_a.0.port_circuit_id") ||
+		d.HasChange("interface_z.0.port_circuit_id") ||
+		d.HasChange("epl") {
+		return diag.Errorf("longhaul_type, port_circuit_id or epl cannot be updated")
 	}
 
 	settings := extractServiceSettings(d)
@@ -192,22 +195,24 @@ func resourceServiceSettingsUpdate(ctx context.Context, d *schema.ResourceData, 
 		if _, err := c.UpdateServiceSettings(vcCID.(string), settings); err != nil {
 			return diag.FromErr(err)
 		}
-		if d.HasChange("subscription_term") || d.HasChange("speed") {
+
+		if d.HasChange("bandwidth.0.speed") || d.HasChange("bandwidth.0.subscription_term") {
 			billing := packetfabric.BillingUpgrade{}
-			if subTerm, ok := d.GetOk("subscription_term"); ok {
+			bandwidth := d.Get("bandwidth.0").(map[string]interface{})
+			if subTerm, ok := bandwidth["subscription_term"]; ok {
 				billing.SubscriptionTerm = subTerm.(int)
 			}
-			if speed, ok := d.GetOk("speed"); ok {
+			if speed, ok := bandwidth["speed"]; ok {
 				billing.Speed = speed.(string)
 			}
 			if _, err := c.ModifyBilling(vcCID.(string), billing); err != nil {
 				return diag.FromErr(err)
 			}
-			if subTerm, ok := d.GetOk("subscription_term"); ok {
-				_ = d.Set("subscription_term", subTerm.(int))
+			if subTerm, ok := bandwidth["subscription_term"]; ok {
+				_ = d.Set("bandwidth.0.subscription_term", subTerm.(int))
 			}
-			if speed, ok := d.GetOk("speed"); ok {
-				_ = d.Set("speed", speed.(string))
+			if speed, ok := bandwidth["speed"]; ok {
+				_ = d.Set("bandwidth.0.speed", speed.(string))
 			}
 		}
 	} else {
@@ -219,58 +224,7 @@ func resourceServiceSettingsUpdate(ctx context.Context, d *schema.ResourceData, 
 	return diags
 }
 
-// used for all Hosted or Dedicated Cloud
-func resourceServicesUpdate(ctx context.Context, d *schema.ResourceData, m interface{}, fn func(string, string) (*packetfabric.CloudServiceConnCreateResp, error)) diag.Diagnostics {
-	c := m.(*packetfabric.PFClient)
-	c.Ctx = ctx
-	var diags diag.Diagnostics
-	cloudCID, ok := d.GetOk("id")
-	if !ok {
-		diags = append(diags, diag.Diagnostic{
-			Severity: diag.Warning,
-			Summary:  "Cloud Service Create",
-			Detail:   cloudCidNotFoundDetailsMsg,
-		})
-		return diags
-	}
-	if desc, ok := d.GetOk("description"); !ok {
-		return diag.Errorf("please provide a valid description for Cloud Service")
-	} else {
-		resp, err := fn(desc.(string), cloudCID.(string))
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		_ = d.Set("description", resp.Description)
-	}
-	// speed only for hosted cloud - subscription_term and service_class only for dedicated cloud
-	if d.HasChange("speed") || d.HasChange("subscription_term") || d.HasChange("service_class") {
-		billing := packetfabric.BillingUpgrade{}
-		if speed, ok := d.GetOk("speed"); ok {
-			billing.Speed = speed.(string)
-		}
-		if subTerm, ok := d.GetOk("subscription_term"); ok {
-			billing.SubscriptionTerm = subTerm.(int)
-		}
-		if serviceClass, ok := d.GetOk("service_class"); ok {
-			billing.ServiceClass = serviceClass.(string)
-		}
-		if _, err := c.ModifyBilling(cloudCID.(string), billing); err != nil {
-			return diag.FromErr(err)
-		}
-		if speed, ok := d.GetOk("speed"); ok {
-			_ = d.Set("speed", speed.(string))
-		}
-		if subTerm, ok := d.GetOk("subscription_term"); ok {
-			_ = d.Set("subscription_term", subTerm.(int))
-		}
-		if serviceClass, ok := d.GetOk("service_class"); ok {
-			_ = d.Set("service_class", serviceClass.(string))
-		}
-	}
-	return diags
-}
-
-// VC Marketplaces don't have an UPDATE mechanism.
+// VC Marketplaces don't have an UPDATE mechanism (both backbone or hosted cloud marketplace)
 func resourceUpdateMarketplace(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	return diag.Diagnostics{diag.Diagnostic{
 		Severity: diag.Error,
--- internal/provider/resource_cloud_service.go
+++ internal/provider/resource_cloud_service.go
@@ -8,6 +8,57 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+// used for all Hosted or Dedicated Cloud
+func resourceServicesUpdate(ctx context.Context, d *schema.ResourceData, m interface{}, fn func(string, string) (*packetfabric.CloudServiceConnCreateResp, error)) diag.Diagnostics {
+	c := m.(*packetfabric.PFClient)
+	c.Ctx = ctx
+	var diags diag.Diagnostics
+	cloudCID, ok := d.GetOk("id")
+	if !ok {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Warning,
+			Summary:  "Cloud Service Create",
+			Detail:   cloudCidNotFoundDetailsMsg,
+		})
+		return diags
+	}
+	if desc, ok := d.GetOk("description"); !ok {
+		return diag.Errorf("please provide a valid description for Cloud Service")
+	} else {
+		resp, err := fn(desc.(string), cloudCID.(string))
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		_ = d.Set("description", resp.Description)
+	}
+	// speed only for hosted cloud - subscription_term and service_class only for dedicated cloud
+	if d.HasChange("speed") || d.HasChange("subscription_term") || d.HasChange("service_class") {
+		billing := packetfabric.BillingUpgrade{}
+		if speed, ok := d.GetOk("speed"); ok {
+			billing.Speed = speed.(string)
+		}
+		if subTerm, ok := d.GetOk("subscription_term"); ok {
+			billing.SubscriptionTerm = subTerm.(int)
+		}
+		if serviceClass, ok := d.GetOk("service_class"); ok {
+			billing.ServiceClass = serviceClass.(string)
+		}
+		if _, err := c.ModifyBilling(cloudCID.(string), billing); err != nil {
+			return diag.FromErr(err)
+		}
+		if speed, ok := d.GetOk("speed"); ok {
+			_ = d.Set("speed", speed.(string))
+		}
+		if subTerm, ok := d.GetOk("subscription_term"); ok {
+			_ = d.Set("subscription_term", subTerm.(int))
+		}
+		if serviceClass, ok := d.GetOk("service_class"); ok {
+			_ = d.Set("service_class", serviceClass.(string))
+		}
+	}
+	return diags
+}
+
 func resourceCloudSourceDelete(ctx context.Context, d *schema.ResourceData, m interface{}, diagSummary string) diag.Diagnostics {
 	c := m.(*packetfabric.PFClient)
 	c.Ctx = ctx
