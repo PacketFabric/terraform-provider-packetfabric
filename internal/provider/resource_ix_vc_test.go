9d1ac64080c40b33a78ae54b961382fd69e78f91
sousafeliciano <felicianosousa@pontovinte.com>
2023-02-21 14:56:09 -0300

    Acc test structure: Resource packetfabric_ix_virtual_circuit_marketplace

--- internal/provider/resource_ix_vc_test.go
+++ internal/provider/resource_ix_vc_test.go
@@ -3,71 +3,67 @@ package provider
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/PacketFabric/terraform-provider-packetfabric/internal/testutil"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func hclIxVc(pop, zone, description, routingID, market, asn, vlan, accountUUID, speed string) (hcl string, resourceName string) {
+func hclIxVc(description, routingId, market, asn, vlan, subscriptionTerm, speed, longhaultype string, untagged bool) (hcl string, resourceName string) {
+
 	portHcl, portResourceName := hclPort(
-		testutil.GenerateUniqueName(testPrefix),
-		accountUUID,
-		speed,
-		"LX",
-		"1",
-		pop,
-		zone,
-		true,
-		false,
+		os.Getenv(testutil.PF_PORT_DESCR),
+		os.Getenv(testutil.PF_PORT_MEDIA_KEY),
+		os.Getenv(testutil.PF_PORT_SPEED_KEY),
+		os.Getenv(testutil.PF_PORT_POP1_KEY),
+		os.Getenv(testutil.PF_PORT_SUBTERM_KEY),
 	)
+
 	hclName := testutil.GenerateUniqueResourceName()
 	resourceName = "packetfabric_ix_virtual_circuit_marketplace." + hclName
-	ixVcHcl := fmt.Sprintf(`
-	resource "packetfabric_ix_virtual_circuit_marketplace" "%s" {
-		description = "%s"
-		routing_id  = "%s"
-		market      = "%s"
-		asn         = "%s"
-		interface {
-			port_circuit_id = %s.id
-			untagged        = false
-			vlan            = "%s"
-		}
-		bandwidth {
-			account_uuid  = "%s"
-			longhaul_type = "usage"
-		}
-	}`, hclName, description, routingID, market, asn, portResourceName, vlan, accountUUID)
-
+	ixVcHcl := fmt.Sprintf(testutil.RResourceIXVirtualCircuitMarketplace, hclName, description, routingId, market, asn,
+		portResourceName, untagged, vlan, longhaultype, speed, subscriptionTerm)
 	hcl = fmt.Sprintf("%s\n%s", portHcl, ixVcHcl)
 	return
 }
 
-func TestAccIxVc(t *testing.T) {
+func TestAccIxVcRequiredFields(t *testing.T) {
 	testutil.SkipIfEnvNotSet(t)
 
-	description := testutil.GenerateUniqueName(testPrefix)
-	pop, zone, err := testutil.GetPopAndZoneWithAvailablePort("1Gbps", "LX")
-	if err != nil {
-		t.Fatalf("Unable to find pop and zone with available port: %s", err)
-	}
+	routingId := os.Getenv(testutil.PF_ROUTING_ID_KEY)
+	description := os.Getenv(testutil.PF_PORT_DESCR)
+	market := os.Getenv(testutil.PF_MARKET_IX_KEY)
+	asn := os.Getenv(testutil.PF_ASN_IX_KEY)
+	vlan := os.Getenv(testutil.PF_VC_VLAN1_KEY)
+	subscriptionTerm := os.Getenv(testutil.PF_VC_SUBTERM_KEY)
+	speed := os.Getenv(testutil.PF_VC_SPEED_KEY)
+	longhaultype := os.Getenv(testutil.PF_VC_LONGHAUL_TYPE_KEY)
+	untagged := testutil.GetEnvBool(testutil.PF_PORT_UNTAGGED_KEY)
+
 	hcl, resourceName := hclIxVc(
-		pop,
-		zone,
 		description,
-		os.Getenv("PF_ACC_TEST_IX_ROUTING_ID"), // TODO(medzin): Refactor to use the data from the customer endpoint.
-		os.Getenv("PF_ACC_TEST_IX_MARKET"),     // TODO(medzin): Refactor to use the data from the customer endpoint.
-		"12345",
-		"5",
-		testutil.GetAccountUUID(),
-		"1Gbps",
+		routingId,
+		market,
+		asn,
+		vlan,
+		subscriptionTerm,
+		speed,
+		longhaultype,
+		untagged,
 	)
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			testutil.PreCheck(t, []string{
-				"PF_ACC_TEST_IX_ROUTING_ID",
-				"PF_ACC_TEST_IX_MARKET",
+				testutil.PF_PORT_DESCR,
+				testutil.PF_ROUTING_ID_IX_KEY,
+				testutil.PF_ASN_IX_KEY,
+				testutil.PF_MARKET_IX_KEY,
+				testutil.PF_VC_SPEED_KEY,
+				testutil.PF_VC_LONGHAUL_TYPE_KEY,
+				testutil.PF_VC_SUBTERM_KEY,
+				testutil.PF_VC_VLAN1_KEY,
+				testutil.PF_PORT_UNTAGGED_KEY,
 			})
 		},
 		Providers: testAccProviders,
@@ -76,9 +72,14 @@ func TestAccIxVc(t *testing.T) {
 				Config: hcl,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "description", description),
-					resource.TestCheckResourceAttr(resourceName, "asn", "12345"),
-					resource.TestCheckResourceAttr(resourceName, "interface.0.vlan", "5"),
-					resource.TestCheckResourceAttr(resourceName, "bandwidth.0.account_uuid", testutil.GetAccountUUID()),
+					resource.TestCheckResourceAttr(resourceName, "routing_id", routingId),
+					resource.TestCheckResourceAttr(resourceName, "asn", asn),
+					resource.TestCheckResourceAttr(resourceName, "market", market),
+					resource.TestCheckResourceAttr(resourceName, "interface.0.vlan", vlan),
+					resource.TestCheckResourceAttr(resourceName, "interface.0.untagged", strconv.FormatBool(untagged)),
+					resource.TestCheckResourceAttr(resourceName, "bandwidth.0.longhaul_type", longhaultype),
+					resource.TestCheckResourceAttr(resourceName, "bandwidth.0.speed", speed),
+					resource.TestCheckResourceAttr(resourceName, "bandwidth.0.subscription_term", subscriptionTerm),
 				),
 			},
 		},
