b05d651b286dd7096bff165d29a2a2d058fc9bf9
solababs <solababatunde12@gmail.com>
2023-02-21 21:22:30 +0100

    update warning message

--- internal/provider/resource_cloud_router_connection.go
+++ internal/provider/resource_cloud_router_connection.go
@@ -96,8 +96,8 @@ func CloudRouterImportStatePassthroughContext(ctx context.Context, d *schema.Res
 func showWarningForUnsetFields(unsetFields []string, diags *diag.Diagnostics) diag.Diagnostics {
 	*diags = append(*diags, diag.Diagnostic{
 		Severity: diag.Warning,
-		Summary:  "Field(s) not set",
-		Detail:   fmt.Sprintf("The following fields: %s cannot not be set. Update the Terraform state file manually if needed.", unsetFields),
+		Summary:  "Field(s) not set.",
+		Detail:   fmt.Sprintf("The following fields: %s cannot be set. Update the Terraform state file manually if needed.", unsetFields),
 	})
 	return *diags
 }
