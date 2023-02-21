a3664d4fc0926710ac156dbfd32b8baa284b06af
sousafeliciano <felicianosousa@pontovinte.com>
2023-02-21 13:57:52 -0300

    Acc test structure: Resource packetfabric_port_loa
    Signed-off-by: sousafeliciano <felicianosousa@pontovinte.com>

--- internal/provider/resource_port_loa_test.go
+++ internal/provider/resource_port_loa_test.go
@@ -2,55 +2,54 @@ package provider
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/PacketFabric/terraform-provider-packetfabric/internal/testutil"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func hclPortLOA(pop, zone, speed, destinationEmail, loaCustomerName string) (hcl string, resourceName string) {
-	portHcl, portResourceName := hclPort(
-		testutil.GenerateUniqueName(testPrefix),
-		testutil.GetAccountUUID(),
-		speed,
-		"LX",
-		"1",
-		pop,
-		zone,
-		true,
-		false,
-	)
+func hclPortLOA(destinationEmail, loaCustomerName string) (hcl string, resourceName string) {
 
+	portHcl, portResourceName := hclPort(
+		os.Getenv(testutil.PF_PORT_DESCR),
+		os.Getenv(testutil.PF_PORT_MEDIA_KEY),
+		os.Getenv(testutil.PF_PORT_SPEED_KEY),
+		os.Getenv(testutil.PF_PORT_POP1_KEY),
+		os.Getenv(testutil.PF_PORT_SUBTERM_KEY),
+	)
 	hclName := testutil.GenerateUniqueResourceName()
 	resourceName = "packetfabric_port_loa." + hclName
-	loaHcl := fmt.Sprintf(`
-	resource "packetfabric_port_loa" "%s" {
-		port_circuit_id   = %s.id
-		destination_email = "%s"
-		loa_customer_name = "%s"
-	}`, hclName, portResourceName, destinationEmail, loaCustomerName)
-
-	hcl = fmt.Sprintf("%s\n%s", portHcl, loaHcl)
+	portLOAhcl := fmt.Sprintf(testutil.RResourcePortLoa, hclName, portResourceName, destinationEmail, loaCustomerName)
+	hcl = fmt.Sprintf("%s\n%s", portHcl, portLOAhcl)
 	return
 }
 
-func TestAccPortLOA(t *testing.T) {
+func TestAccPortLOARequiredFields(t *testing.T) {
 	testutil.SkipIfEnvNotSet(t)
 
-	pop, zone, err := testutil.GetPopAndZoneWithAvailablePort("1Gbps", "LX")
-	if err != nil {
-		t.Fatalf("Unable to find pop and zone with available port: %s", err)
-	}
-	hcl, resourceName := hclPortLOA(pop, zone, "1Gbps", "test@packetfabric.com", "Test Customer")
+	destinationEmail := os.Getenv(testutil.PF_DEST_EMAIL_KEY)
+	loaCustomerName := os.Getenv(testutil.PF_LOA_CUSTOMER_KEY)
+
+	hcl, resourceName := hclPortLOA(
+		destinationEmail,
+		loaCustomerName,
+	)
+
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testutil.PreCheck(t, nil) },
+		PreCheck: func() {
+			testutil.PreCheck(t, []string{
+				testutil.PF_DEST_EMAIL_KEY,
+				testutil.PF_LOA_CUSTOMER_KEY,
+			})
+		},
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: hcl,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "destination_email", "test@packetfabric.com"),
-					resource.TestCheckResourceAttr(resourceName, "loa_customer_name", "Test Customer"),
+					resource.TestCheckResourceAttr(resourceName, "destination_email", destinationEmail),
+					resource.TestCheckResourceAttr(resourceName, "loa_customer_name", loaCustomerName),
 				),
 			},
 		},
