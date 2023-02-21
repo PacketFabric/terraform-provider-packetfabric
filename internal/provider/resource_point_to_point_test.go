413cffd62aee4f538b4ceb1dc9c1de967094e1a7
sousafeliciano <felicianosousa@pontovinte.com>
2023-02-21 14:30:51 -0300

    Acc test structure: Resource packetfabric_point_to_point
    Signed-off-by: sousafeliciano <felicianosousa@pontovinte.com>

--- internal/provider/resource_point_to_point_test.go
+++ internal/provider/resource_point_to_point_test.go
@@ -2,66 +2,76 @@ package provider
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/PacketFabric/terraform-provider-packetfabric/internal/testutil"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func hclPointToPoint(description, accountUUID, speed, media, subscriptionTerm, pop1, zone1, pop2, zone2 string) (hcl string, resourceName string) {
-	hcl = fmt.Sprintf(`
-	resource "packetfabric_point_to_point" "p2p" {
-		description       = "%s"
-		account_uuid      = "%s"
-		speed             = "%s"
-		media             = "%s"
-		subscription_term = %s
-		endpoints {
-			pop     = "%s"
-			zone    = "%s"
-			autoneg = true
-		}
-		endpoints {
-			pop     = "%s"
-			zone    = "%s"
-			autoneg = true
-		}
-	}`, description, accountUUID, speed, media, subscriptionTerm, pop1, zone1, pop2, zone2)
-	resourceName = "packetfabric_point_to_point.p2p"
+func hclPointToPoint(description, speed, media, subscriptionTerm, pop1, zone1, pop2, zone2 string, autoneg1, autonegg2 bool) (hcl string, resourceName string) {
+	hclName := testutil.GenerateUniqueResourceName()
+	resourceName = "packetfabric_point_to_point." + hclName
+	hcl = fmt.Sprintf(testutil.RResourcePointToPoint, hclName, description, speed, media, subscriptionTerm, pop1, zone1, autoneg1, pop2, zone2, autonegg2)
 	return
 }
 
-func TestAccPointToPoint(t *testing.T) {
+func TestAccPointToPointRequiredFields(t *testing.T) {
 	testutil.SkipIfEnvNotSet(t)
 
-	description := testutil.GenerateUniqueName(testPrefix)
+	description := os.Getenv(testutil.PF_PTP_DESCR)
+	speed := os.Getenv(testutil.PF_PTP_SPEED_KEY)
+	media := os.Getenv(testutil.PF_PTP_MEDIA_KEY)
+	subscriptionTerm := os.Getenv(testutil.PF_PTP_SUBTERM_KEY)
+	pop1 := os.Getenv(testutil.PF_PTP_POP1_KEY)
+	zone1 := os.Getenv(testutil.PF_PTP_ZONE1_KEY)
+	pop2 := os.Getenv(testutil.PF_PTP_POP2_KEY)
+	zone2 := os.Getenv(testutil.PF_PTP_ZONE2_KEY)
+	autoneg1 := testutil.GetEnvBool(testutil.PF_PTP_AUTONEG_KEY)
+	autoneg2 := testutil.GetEnvBool(testutil.PF_PTP_AUTONEG_KEY)
+
 	hcl, resourceName := hclPointToPoint(
 		description,
-		testutil.GetAccountUUID(),
-		"1Gbps",
-		"LX",
-		"1",
-		"AMS1",
-		"A",
-		"AMS2",
-		"A",
+		speed,
+		media,
+		subscriptionTerm,
+		pop1,
+		zone1,
+		pop2,
+		zone2,
+		autoneg1,
+		autoneg2,
 	)
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testutil.PreCheck(t, nil) },
+		PreCheck: func() {
+			testutil.PreCheck(t, []string{
+				testutil.PF_PTP_DESCR,
+				testutil.PF_PTP_SPEED_KEY,
+				testutil.PF_PTP_SUBTERM_KEY,
+				testutil.PF_PTP_MEDIA_KEY,
+				testutil.PF_PTP_POP1_KEY,
+				testutil.PF_PTP_ZONE1_KEY,
+				testutil.PF_PTP_POP2_KEY,
+				testutil.PF_PTP_ZONE2_KEY,
+				testutil.PF_PTP_AUTONEG_KEY,
+			})
+		},
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: hcl,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "description", description),
-					resource.TestCheckResourceAttr(resourceName, "account_uuid", testutil.GetAccountUUID()),
-					resource.TestCheckResourceAttr(resourceName, "speed", "1Gbps"),
-					resource.TestCheckResourceAttr(resourceName, "media", "LX"),
-					resource.TestCheckResourceAttr(resourceName, "subscription_term", "1"),
-					resource.TestCheckResourceAttr(resourceName, "endpoints.0.pop", "AMS1"),
-					resource.TestCheckResourceAttr(resourceName, "endpoints.0.zone", "A"),
-					resource.TestCheckResourceAttr(resourceName, "endpoints.1.pop", "AMS2"),
-					resource.TestCheckResourceAttr(resourceName, "endpoints.1.zone", "A"),
+					resource.TestCheckResourceAttr(resourceName, "speed", speed),
+					resource.TestCheckResourceAttr(resourceName, "media", media),
+					resource.TestCheckResourceAttr(resourceName, "subscription_term", subscriptionTerm),
+					resource.TestCheckResourceAttr(resourceName, "endpoints.0.pop", pop1),
+					resource.TestCheckResourceAttr(resourceName, "endpoints.0.zone", zone1),
+					resource.TestCheckResourceAttr(resourceName, "endpoints.0.autoneg", strconv.FormatBool(autoneg1)),
+					resource.TestCheckResourceAttr(resourceName, "endpoints.1.pop", pop2),
+					resource.TestCheckResourceAttr(resourceName, "endpoints.1.zone", zone2),
+					resource.TestCheckResourceAttr(resourceName, "endpoints.0.autoneg", strconv.FormatBool(autoneg2)),
 				),
 			},
 		},
