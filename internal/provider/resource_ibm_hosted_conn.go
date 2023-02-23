7d2bbc9e73974b38cf80b2b04299696635ae461d
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-02-22 20:31:36 -0800

    more renaming

---
+++ internal/provider/resource_ibm_hosted_conn.go
@@ -0,0 +1,240 @@
+package provider
+
+import (
+	"context"
+	"time"
+
+	"github.com/PacketFabric/terraform-provider-packetfabric/internal/packetfabric"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func resourceHostedIbmConn() *schema.Resource {
+	return &schema.Resource{
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Read:   schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(30 * time.Minute),
+		},
+		CreateContext: resourceHostedIbmConnCreate,
+		ReadContext:   resourceHostedIbmConnRead,
+		UpdateContext: resourceHostedIbmConnUpdate,
+		DeleteContext: resourceHostedIbmConnDelete,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ibm_account_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				DefaultFunc:  schema.EnvDefaultFunc("PF_IBM_ACCOUNT_ID", nil),
+				ValidateFunc: validation.StringLenBetween(1, 32),
+				Description: "Your IBM account ID. " +
+					"Can also be set with the PF_IBM_ACCOUNT_ID environment variable.",
+			},
+			"ibm_bgp_asn": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Enter an ASN to use with your BGP session. This should be the same ASN you used for your Cloud Router.",
+			},
+			"ibm_bgp_cer_cidr": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "The IP address in CIDR format for the PacketFabric-side router in the BGP session. If you do not specify an address, IBM will assign one on your behalf.",
+			},
+			"ibm_bgp_ibm_cidr": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "The IP address in CIDR format for the IBM-side router in the BGP session. If you do not specify an address, IBM will assign one on your behalf. See the documentation for information on which IP ranges are allowed.",
+			},
+			"description": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "The description of this connection. This will appear as the connection name from the IBM side. Allows only numbers, letters, underscores and dashes.",
+			},
+			"account_uuid": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				DefaultFunc:  schema.EnvDefaultFunc("PF_ACCOUNT_ID", nil),
+				ValidateFunc: validation.IsUUID,
+				Description: "The UUID for the billing account that should be billed. " +
+					"Can also be set with the PF_ACCOUNT_ID environment variable.",
+			},
+			"pop": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "The POP in which you want to provision the connection (the on-ramp).",
+			},
+			"port": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "The port to connect to IBM.",
+			},
+			"vlan": {
+				Type:         schema.TypeInt,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.IntBetween(4, 4094),
+				Description:  "Valid VLAN range is from 4-4094, inclusive.",
+			},
+			"src_svlan": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "Valid S-VLAN range is from 4-4094, inclusive.",
+			},
+			"speed": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice(speedOptions(), true),
+				Description:  "The speed of the new connection.\n\n\tEnum: [\"50Mbps\" \"100Mbps\" \"200Mbps\" \"300Mbps\" \"400Mbps\" \"500Mbps\" \"1Gbps\" \"2Gbps\" \"5Gbps\" \"10Gbps\"]",
+			},
+			"zone": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "The desired availability zone of the connection.",
+			},
+			"published_quote_line_uuid": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.IsUUID,
+				Description:  "UUID of the published quote line with which this connection should be associated.",
+			},
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+
+func resourceHostedIbmConnCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*packetfabric.PFClient)
+	c.Ctx = ctx
+	var diags diag.Diagnostics
+	ibmConn := extractHostedIBMConn(d)
+	expectedResp, err := c.CreateHostedIBMConn(ibmConn)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	b := make(map[string]interface{})
+	b["ibm"] = expectedResp
+	tflog.Debug(ctx, "\n#### CREATED IBM CONN", b)
+	createOk := make(chan bool)
+	defer close(createOk)
+	ticker := time.NewTicker(10 * time.Second)
+	go func() {
+		for range ticker.C {
+			dedicatedConns, err := c.GetCurrentCustomersHosted()
+			if dedicatedConns != nil && err == nil && len(dedicatedConns) > 0 {
+				for _, conn := range dedicatedConns {
+					if expectedResp.UUID == conn.UUID && conn.State == "active" {
+						expectedResp.CloudCircuitID = conn.CloudCircuitID
+						ticker.Stop()
+						createOk <- true
+					}
+				}
+			}
+		}
+	}()
+	<-createOk
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(expectedResp.CloudCircuitID)
+	return diags
+
+}
+
+func resourceHostedIbmConnRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*packetfabric.PFClient)
+	c.Ctx = ctx
+	var diags diag.Diagnostics
+	resp, err := c.GetCloudConnInfo(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if resp != nil {
+		_ = d.Set("cloud_circuit_id", resp.CloudCircuitID)
+		_ = d.Set("account_uuid", resp.AccountUUID)
+		_ = d.Set("description", resp.Description)
+		_ = d.Set("vlan", resp.Settings.VlanIDPf)
+		_ = d.Set("speed", resp.Speed)
+		_ = d.Set("pop", resp.Pop)
+		_ = d.Set("ibm_account_id", resp.Settings.AccountID)
+		_ = d.Set("ibm_bgp_asn", resp.Settings.BgpAsn)
+		_ = d.Set("ibm_bgp_cer_cidr", resp.Settings.BgpCerCidr)
+		_ = d.Set("ibm_bgp_ibm_cidr", resp.Settings.BgpIbmCidr)
+	}
+	// _unsetFields := []string{"port", "zone", "src_svlan", "published_quote_line_uuid"}
+	// showWarningForUnsetFields(_unsetFields, &diags)
+	return diags
+}
+
+func resourceHostedIbmConnUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*packetfabric.PFClient)
+	return resourceServicesHostedUpdate(ctx, d, m, c.UpdateServiceHostedConn)
+}
+
+func resourceHostedIbmConnDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return resourceCloudSourceDelete(ctx, d, m, "IBM Service Delete")
+}
+
+func extractHostedIBMConn(d *schema.ResourceData) packetfabric.HostedIBMConn {
+	hostedConn := packetfabric.HostedIBMConn{}
+	if accountID, ok := d.GetOk("ibm_account_id"); ok {
+		hostedConn.IbmAccountID = accountID.(string)
+	}
+	if ibmBgpAsn, ok := d.GetOk("ibm_bgp_asn"); ok {
+		hostedConn.IbmBgpAsn = ibmBgpAsn.(int)
+	}
+	if cerCidr, ok := d.GetOk("ibm_bgp_cer_cidr"); ok {
+		hostedConn.IbmBgpCerCidr = cerCidr.(string)
+	}
+	if bgpCidr, ok := d.GetOk("ibm_bgp_ibm_cidr"); ok {
+		hostedConn.IbmBgpIbmCidr = bgpCidr.(string)
+	}
+	if desc, ok := d.GetOk("description"); ok {
+		hostedConn.Description = desc.(string)
+	}
+	if accountUUID, ok := d.GetOk("account_uuid"); ok {
+		hostedConn.AccountUUID = accountUUID.(string)
+	}
+	if pop, ok := d.GetOk("pop"); ok {
+		hostedConn.Pop = pop.(string)
+	}
+	if port, ok := d.GetOk("port"); ok {
+		hostedConn.Port = port.(string)
+	}
+	if vlan, ok := d.GetOk("vlan"); ok {
+		hostedConn.Vlan = vlan.(int)
+	}
+	if srcVlan, ok := d.GetOk("src_vlan"); ok {
+		hostedConn.SrcSvlan = srcVlan.(int)
+	}
+	if zone, ok := d.GetOk("zone"); ok {
+		hostedConn.Zone = zone.(string)
+	}
+	if speed, ok := d.GetOk("speed"); ok {
+		hostedConn.Speed = speed.(string)
+	}
+	return hostedConn
+}
---
+++ internal/provider/resource_service_request_provision.go
@@ -0,0 +1,205 @@
+package provider
+
+import (
+	"context"
+	"time"
+
+	"github.com/PacketFabric/terraform-provider-packetfabric/internal/packetfabric"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+const azureProvider = "azure"
+const googleProvider = "google"
+const oracleProvider = "oracle"
+const awsProvider = "aws"
+
+func resourceProvisionRequestedService() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceProvisionRequestedServiceCreate,
+		ReadContext:   resourceRequestedServiceRead,
+		DeleteContext: resourceRequestedServiceDelete,
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Read:   schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"type": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringInSlice([]string{"backbone", "ix", "cloud"}, true),
+				Description:  "The service type: \"backbone\", \"ix\", or \"cloud\".\n\n\tMost requests will be \"backbone\". Connections to the requesting side's cloud environment are \"cloud\".",
+			},
+			"cloud_provider": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringInSlice([]string{"aws", "google", "oracle", "azure"}, true),
+				Description:  "For cloud connections, this is the cloud provider: \"aws\", \"google\", \"oracle\", \"azure\"",
+			},
+			"vc_request_uuid": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.IsUUID,
+				Description:  "UUID of the connection request you received from the marketplace user.",
+			},
+			"description": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringIsNotEmpty,
+				Description:  "A brief description for the connection.",
+			},
+			"interface": {
+				Type:     schema.TypeSet,
+				Required: true,
+				ForceNew: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"port_circuit_id": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.StringIsNotEmpty,
+							Description:  "The circuit ID of the port on which you want to provision the request. This starts with \"PF-AP-\"",
+						},
+						"vlan": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.IntBetween(4, 4094),
+							Description:  "Valid VLAN range is from 4-4094, inclusive.",
+						},
+						"svlan": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.IntBetween(4, 4094),
+							Description:  "Valid S-VLAN range is from 4-4094, inclusive.",
+						},
+						"vlan_private": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.IntBetween(4, 4094),
+							Description:  "For Microsoft Azure connections with private peerings. Valid VLAN range is from 4-4094, inclusive.",
+						},
+						"vlan_microsoft": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.IntBetween(4, 4094),
+							Description:  "For Microsoft Azure connections with Microsoft (public) peerings. Valid VLAN range is from 4-4094, inclusive.",
+						},
+						"untagged": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							ForceNew:    true,
+							Description: "Whether the interface should be untagged.",
+						},
+					},
+				},
+			},
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+
+func resourceProvisionRequestedServiceCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*packetfabric.PFClient)
+	c.Ctx = ctx
+	var diags diag.Diagnostics
+	provisionReq := extractProvisionRequest(d)
+	vcReqUUID := d.Get("vc_request_uuid")
+	reqType := d.Get("type")
+	switch reqType.(string) {
+	case "cloud":
+		cloudProvider := d.Get("cloud_provider").(string)
+		provisionReq.Provider = cloudProvider
+	}
+	_, err := c.RequestServiceProvision(vcReqUUID.(string), reqType.(string), provisionReq)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(uuid.New().String())
+
+	return diags
+}
+
+func resourceRequestedServiceRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return diag.Diagnostics{diag.Diagnostic{
+		Severity: diag.Warning,
+		Summary:  "Marketplace Request.",
+		Detail:   "Warning: the Marketplace connection request has been either accepted or rejected.",
+	}}
+}
+
+func resourceRequestedServiceDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	d.SetId("")
+	return diag.Diagnostics{}
+}
+
+func extractProvisionRequest(d *schema.ResourceData) packetfabric.ServiceProvision {
+	provisionReq := packetfabric.ServiceProvision{}
+	if desc, ok := d.GetOk("description"); ok {
+		provisionReq.Description = desc.(string)
+	}
+	cloudProvider := "undefined"
+	if cp, ok := d.GetOk("cloud_provider"); ok {
+		cloudProvider = cp.(string)
+	}
+	for _, interfA := range d.Get("interface").(*schema.Set).List() {
+		provisionReq.Interface = extractProvisionInterf(cloudProvider, interfA.(map[string]interface{}))
+	}
+	return provisionReq
+}
+
+func extractProvisionInterf(cloudProvider string, interf map[string]interface{}) packetfabric.Interface {
+	provisionInterf := packetfabric.Interface{}
+	provisionInterf.PortCircuitID = interf["port_circuit_id"].(string)
+	switch cloudProvider {
+	case awsProvider, googleProvider, oracleProvider:
+		provisionInterf.Vlan = interf["vlan"].(int)
+	case azureProvider:
+		provisionInterf.VlanMicrosoft = interf["vlan_microsoft"].(int)
+		provisionInterf.VlanPrivate = interf["vlan_private"].(int)
+	default:
+		provisionInterf.Vlan = interf["vlan"].(int)
+		provisionInterf.Svlan = interf["svlan"].(int)
+		provisionInterf.Untagged = interf["untagged"].(bool)
+	}
+	return provisionInterf
+}
+
+func extractProvision(d *schema.ResourceData, provider string) packetfabric.ServiceAwsMktConn {
+	mktConn := packetfabric.ServiceAwsMktConn{Provider: provider}
+	interf := packetfabric.ServiceAwsInterf{}
+	if portCid, ok := d.GetOk("port_circuit_id"); ok {
+		interf.PortCircuitID = portCid.(string)
+	}
+	if vlan, ok := d.GetOk("vlan"); ok {
+		interf.Vlan = vlan.(int)
+	}
+	if vlanMicrosoft, ok := d.GetOk("vlan_microsoft"); ok {
+		interf.VlanMicrosoft = vlanMicrosoft.(int)
+	}
+	if vlanPriv, ok := d.GetOk("vlan_private"); ok {
+		interf.VlanPrivate = vlanPriv.(int)
+	}
+	if desc, ok := d.GetOk("description"); ok {
+		mktConn.Description = desc.(string)
+	}
+	mktConn.Interface = interf
+	return mktConn
+}
