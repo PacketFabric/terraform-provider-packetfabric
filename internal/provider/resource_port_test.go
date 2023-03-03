b3e9f1e4f944ee43da6539cee74b6cac1a4e4860
Yrineu Rodrigues <yrineu@pontovinte.com>
2023-03-03 14:48:43 -0500

    ACCTest for packetfabric_port
    Signed-off-by: Yrineu Rodrigues <yrineu@pontovinte.com>

---
+++ internal/provider/resource_port_test.go
@@ -0,0 +1,43 @@
+package provider
+
+import (
+	"log"
+	"strconv"
+	"testing"
+
+	"github.com/PacketFabric/terraform-provider-packetfabric/internal/testutil"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccPort(t *testing.T) {
+	testutil.SkipIfEnvNotSet(t)
+
+	portDetails := testutil.CreateBasePortDetails()
+	portTestResult := portDetails.RHclPort()
+	log.Println(portTestResult.ResourceReference)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testutil.PreCheck(t, []string{})
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:             portTestResult.Hcl,
+				ExpectNonEmptyPlan: true,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(portTestResult.ResourceReference, "description", portTestResult.Description),
+					resource.TestCheckResourceAttr(portTestResult.ResourceReference, "speed", portTestResult.Speed),
+					resource.TestCheckResourceAttr(portTestResult.ResourceReference, "media", portTestResult.Media),
+					resource.TestCheckResourceAttr(portTestResult.ResourceReference, "subscription_term", strconv.Itoa(portTestResult.SubscriptionTerm)),
+					resource.TestCheckResourceAttr(portTestResult.ResourceReference, "pop", portTestResult.Pop),
+					resource.TestCheckResourceAttr(portTestResult.ResourceReference, "enabled", strconv.FormatBool(portTestResult.Enabled)),
+				),
+			},
+			{
+				ResourceName:      portTestResult.ResourceReference,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
