6b4f4e474bc1a621ec3b52e0063959e0ca479b65
sousafeliciano <felicianosousa@pontovinte.com>
2023-03-08 21:40:59 -0300

    Acc test structure: Resource packetfabric_cloud_router_connection_azure

---
+++ internal/provider/resource_azure_cloud_router_connection_test.go
@@ -0,0 +1,35 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/PacketFabric/terraform-provider-packetfabric/internal/testutil"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccAzureCloudRouterConnectionRequiredFields(t *testing.T) {
+
+	testutil.SkipIfEnvNotSet(t)
+
+	cloudRouterConnectionAzureResult := testutil.RHclCloudRouterConnectionAzure()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testutil.PreCheck(t, []string{
+				testutil.PF_ACCOUNT_ID_KEY,
+			})
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: cloudRouterConnectionAzureResult.Hcl,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(cloudRouterConnectionAzureResult.ResourceName, "azure_service_key", cloudRouterConnectionAzureResult.AzureServiceKey),
+					resource.TestCheckResourceAttr(cloudRouterConnectionAzureResult.ResourceName, "description", cloudRouterConnectionAzureResult.Desc),
+					resource.TestCheckResourceAttr(cloudRouterConnectionAzureResult.ResourceName, "speed", cloudRouterConnectionAzureResult.Speed),
+				),
+			},
+		},
+	})
+
+}
--- internal/testutil/test_hcls.go
+++ internal/testutil/test_hcls.go
@@ -20,6 +20,7 @@ const pfCloudRouter = "packetfabric_cloud_router"
 const pfCloudRouterConnAws = "packetfabric_cloud_router_connection_aws"
 const pfCloudRouterBgpSession = "packetfabric_cloud_router_bgp_session"
 const pfCsAwsHostedConn = "packetfabric_cs_aws_hosted_connection"
+const pfCloudRouterConnAzure = "packetfabric_cloud_router_connection_azure"
 
 // ########################################
 // ###### HARDCODED VALUES
@@ -116,6 +117,15 @@ type RHclBgpSessionResult struct {
 	Type1              string
 	Prefix2            string
 	Type2              string
+}
+
+// packetfabric_cloud_router_connection_azure
+type RHclCloudRouterConnectionAzureResult struct {
+	HclResultBase
+	Desc            string
+	CloudRouter     RHclCloudRouterResult
+	AzureServiceKey string
+	Speed           string
 }
 
 // Patterns:
@@ -329,6 +339,25 @@ func RHclAwsHostedConnection() RHclCloudRouterConnectionAwsResult {
 	}
 }
 
+// packetfabric_cloud_router_connection_azure
+func RHclCloudRouterConnectionAzure() RHclCloudRouterConnectionAzureResult {
+
+	var azureServiceKey, speed, hcl string
+
+	uniqueDesc := _generateUniqueNameOrDesc(pfCloudRouterConnAzure)
+	resourceName, _ := _generateResourceName(pfCloudRouterConnAzure)
+
+	return RHclCloudRouterConnectionAzureResult{
+		HclResultBase: HclResultBase{
+			Hcl:          hcl,
+			Resource:     pfCloudRouterConnAzure,
+			ResourceName: resourceName,
+		},
+		Desc:            uniqueDesc,
+		AzureServiceKey: azureServiceKey,
+		Speed:           speed,
+	}
+}
 func (details PortDetails) _findAvailableCloudPopZoneAndMedia() (pop, zone, media string) {
 	popsAvailable, _ := details.FetchCloudPops()
 	popsToSkip := make([]string, 0)
