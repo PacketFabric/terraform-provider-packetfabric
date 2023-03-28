b6a1b63dbf0bde2a68a8e6f33e533aa79243a55c
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-03-28 11:05:39 -0700

    add labels to vc example

--- examples/resources/packetfabric_backbone_virtual_circuit/resource.tf
+++ examples/resources/packetfabric_backbone_virtual_circuit/resource.tf
@@ -8,6 +8,7 @@ resource "packetfabric_port" "port_1" {
   speed             = "1Gbps"
   subscription_term = 1
   zone              = "A"
+  labels            = ["terraform", "dev"]
 }
 
 resource "packetfabric_port" "port_2" {
@@ -20,6 +21,7 @@ resource "packetfabric_port" "port_2" {
   speed             = "1Gbps"
   subscription_term = 1
   zone              = "A"
+  labels            = ["terraform", "dev"]
 }
 
 resource "packetfabric_backbone_virtual_circuit" "vc1" {
@@ -41,4 +43,5 @@ resource "packetfabric_backbone_virtual_circuit" "vc1" {
     speed             = "1Gbps"
     subscription_term = 1
   }
+  labels = ["terraform", "dev"]
 }
