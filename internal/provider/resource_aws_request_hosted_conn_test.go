c57d851e7bcec9c8a54baf1360a033c3e3c66579
sousafeliciano <felicianosousa@pontovinte.com>
2023-03-31 16:54:43 -0300

    Acc HCL test structure: Resource packetfabric_cs_aws_hosted_connection

---
+++ internal/provider/resource_aws_request_hosted_conn_test.go
@@ -0,0 +1,35 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/PacketFabric/terraform-provider-packetfabric/internal/testutil"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccAwsHostedConnectionRequiredFields(t *testing.T) {
+
+	testutil.SkipIfEnvNotSet(t)
+
+	awsHostedConnectionResult := testutil.RHclAwsHostedConnection()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testutil.PreCheck(t, []string{
+				testutil.PF_CRC_AWS_ACCOUNT_ID_KEY,
+				testutil.PF_ACCOUNT_ID_KEY,
+			})
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:             awsHostedConnectionResult.Hcl,
+				ExpectNonEmptyPlan: true,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(awsHostedConnectionResult.ResourceName, "pop", awsHostedConnectionResult.Pop),
+					resource.TestCheckResourceAttr(awsHostedConnectionResult.ResourceName, "description", awsHostedConnectionResult.Desc),
+				),
+			},
+		},
+	})
+}
