7990c884c1c30e26c01c8183872221be73a0d4d9
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-04-05 07:21:57 -0700

    Make google_vlan_attachment_name optional when Cloud Settings are used

--- examples/resources/packetfabric_cs_google_hosted_connection/resource.tf
+++ examples/resources/packetfabric_cs_google_hosted_connection/resource.tf
@@ -23,7 +23,6 @@ resource "packetfabric_cs_aws_hosted_connection" "cs_conn1_hosted_aws_cloud_side
   description                 = "hello world"
   port                        = packetfabric_port.port_1.id
   speed                       = "10Gbps"
-  google_vlan_attachment_name = "my-google-vlan-attachment-primary"
   pop                         = "BOS1"
   vlan                        = 102
   zone                        = "A"
