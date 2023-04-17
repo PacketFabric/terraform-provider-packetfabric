ecf1cbec48066bd105f2103a7a8afb8d4b2990a5
Romain Jouhannet <25542366+rjouhann@users.noreply.github.com>
2023-04-17 08:25:31 -0700

    Check if po_number is set in the resource using po_number (#435)
    * Check if po_number is se in the resource
    
    * remove duplicates
    
    * fix lint error

--- internal/provider/resource_azure_hosted_conn.go
+++ internal/provider/resource_azure_hosted_conn.go
@@ -162,7 +162,9 @@ func resourceAzureReqExpressHostedConnRead(ctx context.Context, d *schema.Resour
 		_ = d.Set("azure_service_key", resp.Settings.AzureServiceKey)
 		_ = d.Set("vlan_private", resp.Settings.VlanIDPrivate)
 		_ = d.Set("vlan_microsoft", resp.Settings.VlanIDMicrosoft)
-		_ = d.Set("po_number", resp.PONumber)
+		if _, ok := d.GetOk("po_number"); ok {
+			_ = d.Set("po_number", resp.PONumber)
+		}
 	}
 	resp2, err2 := c.GetBackboneByVcCID(d.Id())
 	if err2 != nil {
