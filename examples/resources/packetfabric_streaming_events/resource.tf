2f06ac3ae2dac4dd0f7536be1d87d2ff5bccb1c7
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-04-17 12:23:28 -0700

    update docs and example

--- examples/resources/packetfabric_streaming_events/resource.tf
+++ examples/resources/packetfabric_streaming_events/resource.tf
@@ -1,32 +1,23 @@
 resource "packetfabric_streaming_events" "example" {
-  provider = packetfabric
+  provider    = packetfabric
   streams {
     type   = "customer"
     events = ["auth", "physical_interface"]
   }
-
   streams {
     type   = "port"
-    ifds   = ["PF-AP-ABC-1234", "PF-AE-DEF-5678"]
+    ifds   = ["PF-AP-LAB1-2999387", "PF-AP-LAB6-3001683"]
     events = ["errors", "etherstats", "metrics", "optical"]
   }
+  # streams {
+  #   type   = "vc"
+  #   vcs    = ["PF-BC-NYC-LAB-3011206-PF&PF-AP-LAB6-3001683", "PF-BC-NYC-LAB-3011206-PF&PF-AP-LAB1-2999387"]
+  #   events = ["metrics"]
+  # }
+}
 
-  streams {
-    type   = "vc"
-    vcs    = ["PF-BC-ABC-QRS-84274-PF&PF-AP-ABC-1234", "PF-CC-JKL-DEF-13758-PF&PF-AE-DEF-5678"]
-    events = ["metrics"]
+resource "null_resource" "stream_events" {
+  provisioner "local-exec" {
+    command = "python3 packetfabric_streaming_events.py --subscription_uuid ${packetfabric_streaming_events.example.id} --duration_seconds 60 --output_file pf_events.json"
   }
-}
-
-data "packetfabric_streaming_events" "example" {
-  provider        = packetfabric
-  subscription_id = packetfabric_streaming_events.example.id
-  stream_time     = 1 # min
-}
-output "packetfabric_streaming_events_result" {
-  value = data.packetfabric_streaming_events.example.events
-}
-resource "local_file" "packetfabric_streaming_events_output" {
-  content  = jsonencode(data.packetfabric_streaming_events.example.events)
-  filename = "packetfabric_streaming_events_output.json"
 }
--- examples/test.tf
+++ examples/test.tf
@@ -1113,16 +1113,21 @@ resource "random_pet" "name" {}
 #     type   = "customer"
 #     events = ["auth", "physical_interface"]
 #   }
-
 #   streams {
 #     type   = "port"
 #     ifds   = ["PF-AP-LAB1-2999387", "PF-AP-LAB6-3001683"]
 #     events = ["errors", "etherstats", "metrics", "optical"]
 #   }
+#   # streams {
+#   #   type   = "vc"
+#   #   vcs    = ["PF-BC-NYC-LAB-3011206-PF&PF-AP-LAB6-3001683", "PF-BC-NYC-LAB-3011206-PF&PF-AP-LAB1-2999387"]
+#   #   events = ["metrics"]
+#   # }
+# }
 
-#   streams {
-#     type   = "vc"
-#     vcs    = ["PF-BC-NYC-LAB-3011206-PF&PF-AP-LAB6-3001683", "PF-BC-NYC-LAB-3011206-PF&PF-AP-LAB1-2999387"]
-#     events = ["metrics"]
+# resource "null_resource" "stream_events" {
+#   provisioner "local-exec" {
+#     command = "python3 packetfabric_streaming_events.py --subscription_uuid ${packetfabric_streaming_events.example.id} --duration_seconds 60 --output_file pf_events.json"
 #   }
 # }
+
