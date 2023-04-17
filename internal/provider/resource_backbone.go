11b855c80eeff992eafe8be26b84fe00f99f8807
Romain Jouhannet <25542366+rjouhann@users.noreply.github.com>
2023-04-17 08:22:45 -0700

    Check if Labels are set and fix getLabel in all resources using labels (#434)
    * Check if Labels are set and fix getLabel in all resources using labels
    
    * Add sort labels
    
    * reverse commit

--- internal/provider/resource_backbone.go
+++ internal/provider/resource_backbone.go
@@ -296,11 +296,13 @@ func resourceBackboneRead(ctx context.Context, d *schema.ResourceData, m interfa
 		_ = d.Set("po_number", resp.PONumber)
 	}
 
-	labels, err2 := getLabels(c, d.Id())
-	if err2 != nil {
-		return diag.FromErr(err2)
+	if _, ok := d.GetOk("labels"); ok {
+		labels, err2 := getLabels(c, d.Id())
+		if err2 != nil {
+			return diag.FromErr(err2)
+		}
+		_ = d.Set("labels", labels)
 	}
-	_ = d.Set("labels", labels)
 	return diags
 }
 
