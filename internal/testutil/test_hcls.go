aea104cd4936cc79c2f2cfe293d9d4c2fa4091c9
sousafeliciano <felicianosousa@pontovinte.com>
2023-04-17 22:14:23 -0300

    Generating HCL for datasources packetfabric_activitylog

--- internal/testutil/test_hcls.go
+++ internal/testutil/test_hcls.go
@@ -20,6 +20,7 @@ const pfCloudRouter = "packetfabric_cloud_router"
 const pfCloudRouterConnAws = "packetfabric_cloud_router_connection_aws"
 const pfCloudRouterBgpSession = "packetfabric_cloud_router_bgp_session"
 const pfCsAwsHostedConn = "packetfabric_cs_aws_hosted_connection"
+const pfDataActivityLog = "data.packetfabric_activitylog"
 
 // ########################################
 // ###### HARDCODED VALUES
@@ -115,6 +116,11 @@ type RHclBgpSessionResult struct {
 	Type1              string
 	Prefix2            string
 	Type2              string
+}
+
+// data packetfabric_activitylog
+type DHclDatasourceActivityLogResult struct {
+	HclResultBase
 }
 
 // Patterns:
@@ -325,6 +331,23 @@ func RHclAwsHostedConnection() RHclCloudRouterConnectionAwsResult {
 		AwsAccountID: os.Getenv(PF_CRC_AWS_ACCOUNT_ID_KEY),
 		Desc:         uniqueDesc,
 		Pop:          pop,
+	}
+}
+
+func DHclDataSourceActivityLog() DHclDatasourceActivityLogResult {
+
+	resourceName, hclName := _generateResourceName(pfDataActivityLog)
+	hcl := fmt.Sprintf(
+		DDatasourceActivityLog,
+		hclName,
+	)
+
+	return DHclDatasourceActivityLogResult{
+		HclResultBase: HclResultBase{
+			Hcl:          hcl,
+			Resource:     pfDataActivityLog,
+			ResourceName: resourceName,
+		},
 	}
 }
 
--- internal/testutil/test_schemas.go
+++ internal/testutil/test_schemas.go
@@ -405,3 +405,6 @@ const RResourcePortLoa = `resource "packetfabric_port_loa" "%s" {
 }`
 
 // End of resources templates for required fields only
+const DDatasourceActivityLog = `data "packetfabric_activitylog" "%s" {
+  provider = packetfabric
+  }`
