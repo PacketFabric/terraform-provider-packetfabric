a4e92ac4a5c67a8a4e287d49ea3d5f95c3da0efb
Yrineu Rodrigues <yrineu@pontovinte.com>
2023-04-21 23:48:06 -0400

    Fix dependency references.
    Signed-off-by: Yrineu Rodrigues <yrineu@pontovinte.com>

--- internal/testutil/test_hcls.go
+++ internal/testutil/test_hcls.go
@@ -20,7 +20,7 @@ const pfCloudRouter = "packetfabric_cloud_router"
 const pfCloudRouterConnAws = "packetfabric_cloud_router_connection_aws"
 const pfCloudRouterBgpSession = "packetfabric_cloud_router_bgp_session"
 const pfCsAwsHostedConn = "packetfabric_cs_aws_hosted_connection"
-const pfDataVcRequests = "data.packetfabric_marketplace_service_port_requests."
+const pfDataVcRequests = "data.packetfabric_marketplace_service_port_requests"
 
 // ########################################
 // ###### HARDCODED VALUES
