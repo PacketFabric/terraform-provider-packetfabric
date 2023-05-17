17c14de672cf92b0e890c89c2a1e4cc2f65c4b55
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-05-17 07:21:52 -0700

    merge main

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -14,7 +14,6 @@ var testAccExternalProviders = map[string]resource.ExternalProvider{
 		VersionConstraint: "4.61.0",
 		Source:            "hashicorp/google",
 	},
-<<<<<<< HEAD
 	"azurerm": {
 		VersionConstraint: "3.56.0",
 		Source:            "hashicorp/azurerm",
@@ -42,8 +41,6 @@ var testAccExternalProviders = map[string]resource.ExternalProvider{
 		VersionConstraint: "4.111.0",
 		Source:            "oracle/oci",
 	},
-=======
->>>>>>> main
 }
 
 var testAccProviders = map[string]*schema.Provider{
