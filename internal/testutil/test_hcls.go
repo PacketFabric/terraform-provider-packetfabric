4e7f322a2754ddd48a54d0a7833e14911b29b2b7
Romain Jouhannet <25542366+rjouhann@users.noreply.github.com>
2023-05-18 07:17:12 -0700

    Google ACC test Edge Availability selection (#511)

--- internal/testutil/test_hcls.go
+++ internal/testutil/test_hcls.go
@@ -889,6 +889,8 @@ func RHclCloudRouterConnectionAws() RHclCloudRouterConnectionAwsResult {
 // packetfabric_cloud_router_connection_google
 func RHclCloudRouterConnectionGoogle() RHclCloudRouterConnectionGoogleResult {
 
+	var edgeAvailabilityDomain string
+
 	c, err := _createPFClient()
 	if err != nil {
 		log.Panic(err)
@@ -909,11 +911,19 @@ func RHclCloudRouterConnectionGoogle() RHclCloudRouterConnectionGoogleResult {
 	uniqueDesc := GenerateUniqueName()
 	log.Printf("Resource name: %s, description: %s\n", hclName, uniqueDesc)
 
+	if pop == "LAB1" {
+		edgeAvailabilityDomain = "AVAILABILITY_DOMAIN_2"
+	} else {
+		edgeAvailabilityDomain = "AVAILABILITY_DOMAIN_1"
+	}
+	log.Printf("Edge Availability Domain %s\n", edgeAvailabilityDomain)
+
 	crcHcl := fmt.Sprintf(
 		RResourceCloudRouterConnectionGoogle,
 		CloudRouterConnGoogleRegion,
 		CloudRouterConnGoogleNetwork,
 		CloudRouterConnGoogleRegion,
+		edgeAvailabilityDomain,
 		hclName,
 		hclCloudRouterRes.ResourceName,
 		os.Getenv("PF_ACCOUNT_ID"),
@@ -1167,6 +1177,8 @@ func RHclCsAwsHostedConnection() RHclCsHostedCloudAwsResult {
 // packetfabric_cs_google_hosted_connection
 func RHclCsGoogleHostedConnection() RHclCsHostedCloudGoogleResult {
 
+	var edgeAvailabilityDomain string
+
 	c, err := _createPFClient()
 	if err != nil {
 		log.Panic(err)
@@ -1187,11 +1199,19 @@ func RHclCsGoogleHostedConnection() RHclCsHostedCloudGoogleResult {
 	uniqueDesc := GenerateUniqueName()
 	log.Printf("Resource name: %s, description: %s\n", hclName, uniqueDesc)
 
+	if pop == "LAB1" {
+		edgeAvailabilityDomain = "AVAILABILITY_DOMAIN_2"
+	} else {
+		edgeAvailabilityDomain = "AVAILABILITY_DOMAIN_1"
+	}
+	log.Printf("Edge Availability Domain %s\n", edgeAvailabilityDomain)
+
 	googleHostedConnectionHcl := fmt.Sprintf(
 		RResourceCSGoogleHostedConnection,
 		CloudRouterConnGoogleRegion,
 		CloudRouterConnGoogleNetwork,
 		CloudRouterConnGoogleRegion,
+		edgeAvailabilityDomain,
 		hclName,
 		portTestResult.ResourceName,
 		os.Getenv("PF_ACCOUNT_ID"),
