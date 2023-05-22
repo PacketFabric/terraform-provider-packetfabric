88022baf515be3d9f41225fd8a60c4950f43f23d
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2023-05-22 11:07:13 -0700

    add rules for naming

--- examples/use-cases/dedicated_cloud_aws/variables.tf
+++ examples/use-cases/dedicated_cloud_aws/variables.tf
@@ -1,4 +1,7 @@
 ## General VARs
+# Must follow ^(?:[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?)$
+# Any lowercase ASCII letter or digit, and possibly hyphen, which should start with a letter and end with a letter or digit, 
+# and have at most 63 characters (1 for the starting letter + up to 61 characters in the middle + 1 for the ending letter/digit).
 variable "resource_name" {
   type        = string
   description = "Used to name all resources created in this example"
