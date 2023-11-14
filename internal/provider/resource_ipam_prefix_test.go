5fba1c6d45880a9725ca3d3b8cd164cb8b8c6712
Valerie GvM <valerie.grafin-von-mainberg@packetfabric.com>
2023-11-14 11:41:54 -0500

    fix typo in test name

--- internal/provider/resource_ipam_prefix_test.go
+++ internal/provider/resource_ipam_prefix_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccIpamPrefixequiredFields(t *testing.T) {
+func TestAccIpamPrefixRequiredFields(t *testing.T) {
 	testutil.PreCheck(t, nil)
 
 	ipamPrefixResult := testutil.RHclIpamPrefix()
