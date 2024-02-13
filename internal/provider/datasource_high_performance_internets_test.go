5791877efcc353e6d046df33855d297b9ebb70cf
Valerie GvM <valerie.grafin-von-mainberg@packetfabric.com>
2024-02-13 12:02:40 -0500

    fix fields verified and remove hcl output

--- internal/provider/datasource_high_performance_internets_test.go
+++ internal/provider/datasource_high_performance_internets_test.go
@@ -3,7 +3,6 @@
 package provider
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/PacketFabric/terraform-provider-packetfabric/internal/testutil"
@@ -14,7 +13,6 @@ func TestAccDataSourceHighPerformanceInternetsComputedRequiredFields(t *testing.
 	testutil.PreCheck(t, nil)
 
 	hclHighPerformanceInternetResult := testutil.DHclHighPerformanceInternets()
-	fmt.Printf(">>>>>%s\n<<<<<", hclHighPerformanceInternetResult.Hcl)
 
 	resource.ParallelTest(t, resource.TestCase{
 		Providers: testAccProviders,
@@ -22,16 +20,14 @@ func TestAccDataSourceHighPerformanceInternetsComputedRequiredFields(t *testing.
 			{
 				Config: hclHighPerformanceInternetResult.Hcl,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.address"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.country_code"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.apnic_org_id"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.apnic_ref"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.name"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.email"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.phone"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.ripe_org_id"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.ripe_ref"),
-					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.uuid"),
+					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.circuit_id"),
+					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.port_circuit_id"),
+					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.speed"),
+					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.vlan"),
+					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.description"),
+					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.market"),
+					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.routing_type"),
+					resource.TestCheckResourceAttrSet(hclHighPerformanceInternetResult.ResourceName, "high_performance_internets.0.state"),
 				),
 			},
 		},
